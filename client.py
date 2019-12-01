37b7592fe949df51d9e4c4c70d33d3a050c8080d
Catherine Simard <catherine.simard.30@ulaval.ca>
2019-12-01 12:07:00 -0500

    ajout des librairies et config.txt

--- client.py
+++ client.py
@@ -1,3 +1,6 @@
+import serveur
+import getpass
+
 def choix_de_laction():
     print("Menu de connexion")
     print("1. Creer un compte")
@@ -5,20 +8,33 @@ def choix_de_laction():
     choix = input()
     return choix
 
-
 def nouveau_compte():
     print("Menu creation de compte")
-    choix = input()
+    print("Entrez un nom d'usager")
+    nomUsager = input()
+    print("Entrez un mot de passe")
+    motDePasse = input()
+    ##valide = verifierValiditeNouveauCompte(nomUsager, motDePasse)
 
 
 def connection_utilisateur():
     print("Connexion de l'utilisateur")
-    choix = input()
+    print("Entrez votre nom d'usager")
+    nomUsager = input()
+    ##print("Entrez votre mot de passe")
+    ##motDePasse = input()
+    ##valide = verifierValiditeCompteExistant(nomUsager, motDePasse)
+    ##if valide :
+
+
+
 
 
 if __name__ == "__main__":
     action = int(choix_de_laction())
     if action == 1:
         nouveau_compte()
+
     if action == 2:
         connection_utilisateur()
+
