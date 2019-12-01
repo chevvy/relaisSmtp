dc02216325444b7589a00b0db718ccd39fc55130
SolZanettol <leoguerinmorneau@gmail.com>
2019-12-01 12:01:32 -0500

    Fonction mdp_est_conforme déplacé

--- client.py
+++ client.py
@@ -1,3 +1,5 @@
+import re
+
 def choix_de_laction():
     print("Menu de connexion")
     print("1. Creer un compte")
@@ -22,3 +24,16 @@ if __name__ == "__main__":
         nouveau_compte()
     if action == 2:
         connection_utilisateur()
+
+def mdp_est_conforme(mdp):
+    if re.search(r"\s", mdp):
+        return False, "Le mot de passe ne doit pas contenir d'espace ou de tabulation."
+    if not re.search(r"(\S){6}", mdp) or re.search(r"(\S){13}", mdp):
+        return False, "Le mot de passe doit contenir entre 6 et 12 caractères."
+    if not re.search(r"\S*\d\S*\d\S*", mdp):
+        return False, "Le mot de passe doit contenir au moins deux chiffres."
+    if not re.search(r"[A-Z]", mdp):
+        return False, "Le mot de passe doit contenir au moins une lettre majuscule."
+    if not re.search(r"[a-z]", mdp):
+        return False, "Le mot de passe doit contenir au moins une lettre minuscule."
+    return True, "ok"
--- serveur.py
+++ serveur.py
@@ -1,14 +1,2 @@
-import re
 
-def mdp_est_conforme(mdp):
-    if re.search(r"\s", mdp):
-        return False, "Le mot de passe ne doit pas contenir d'espace ou de tabulation."
-    if not re.search(r"(\S){6}", mdp) or re.search(r"(\S){13}", mdp):
-        return False, "Le mot de passe doit contenir entre 6 et 12 caractères."
-    if not re.search(r"\S*\d\S*\d\S*", mdp):
-        return False, "Le mot de passe doit contenir au moins deux chiffres."
-    if not re.search(r"[A-Z]", mdp):
-        return False, "Le mot de passe doit contenir au moins une lettre majuscule."
-    if not re.search(r"[a-z]", mdp):
-        return False, "Le mot de passe doit contenir au moins une lettre minuscule."
-    return True, "ok"
+
